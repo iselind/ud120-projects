d1cb59d6cb5c798e2fb685f1a2c8572370f5ce02
Patrik Iselind <patriki@axis.com>
2017-05-23 15:20:38 +0200

    fix formatting and pylint warnings
    Change-Id: I60be3e69e898655e537d157e05e1aefc3a6b54eb

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,41 +1,48 @@
 #!/usr/bin/python
+'''
+foo bar
+'''
 
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
+# pylint: disable=invalid-name
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
-### the training data (features_train, labels_train) have both "fast" and "slow"
-### points mixed together--separate them so we can give them different colors
-### in the scatterplot and identify them visually
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+# the training data (features_train, labels_train) have both "fast" and "slow"
+# points mixed together--separate them so we can give them different colors
+# in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+bumpy_fast = [features_train[ii][1]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+grade_slow = [features_train[ii][0]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 1]
+bumpy_slow = [features_train[ii][1]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 1]
 
 
-#### initial visualization
+# initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.scatter(bumpy_fast, grade_fast, color="b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color="r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
-################################################################################
+##########################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+# your code here!  name your classifier object clf if you want the
+# visualization code (prettyPicture) to show you the decision boundary
 
 
-
-
-
-
+# K-nearest neighbors
+# random forest
+# adaboost (sometimes also called boosted decision tree)
 
 
 try:
